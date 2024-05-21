bf0a1a5aaec6c5d96f26ce40cbdda7577a78c623
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-05-21 16:53:45 -0400

    Add: Estilos angular + uso de preprocesador scss; Change: FullLayoutComponent

--- src/campeonato-frontend/src/app/views/main/main.component.ts
+++ src/campeonato-frontend/src/app/views/main/main.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatListModule],
   templateUrl: './main.component.html',
   styles: ``
 })
 export class MainComponent {
   contador: number = 0
+
+  constructor(){
+    console.log("Iniciando MainComponent");
+  }
 
   incrementar(){
     this.contador = this.contador + 1
